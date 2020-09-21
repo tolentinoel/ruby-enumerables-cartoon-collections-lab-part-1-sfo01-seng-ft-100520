50b5f9a7a053e208902fd1b29d53de2c6898ea97
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-21 18:25:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,8 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index { |item, index|
-  index = array[item]
-  index += 1
-  puts "#{array[item]}"
+  puts array[item]
+
 }
 
 end
